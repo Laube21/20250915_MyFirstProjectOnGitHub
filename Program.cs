f5f0a90d6222b8dea6d5a8cd1f9fb321c023c207
Laube21 <sebi.laubichler@gmail.com>
2025-09-16 12:32:26 +0200

    Test#1

--- Program.cs
+++ Program.cs
@@ -8,3 +8,7 @@ Console.WriteLine("add_something #3");
 Console.WriteLine("new add_something #1");
 
 Console.WriteLine("Master continue");
+
+Console.WriteLine("Test1");
+
+Console.WriteLine("Test #1");
